0aaa251563de6ac249b25bae08119447e325ac6d
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-02-12 00:02:17 +0600

    Updated Recoverd Items section

--- src/pages/allRecovered/AllRecovered.jsx
+++ src/pages/allRecovered/AllRecovered.jsx
@@ -10,6 +10,7 @@ import { FaBars } from "react-icons/fa";
 import { useState } from "react";
 import RecoveredCard from "./RecoveredCard";
 import { Helmet } from "react-helmet";
+import Container from "../../components/container/Container";
 
 const AllRecovered = () => {
   const { isDark, user } = useContextValue();
@@ -39,80 +40,77 @@ const AllRecovered = () => {
       confirmButtonText: "Try again",
     });
 
-
   if (isPending) return <Loading />;
 
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>Recovered Items | Track & Retrieve</title>
       </Helmet>
-    <section className={`${isDark && "dark"}`}>
-      <section className="px-5 w-full pb-16 pt-10 bg-white dark:bg-darkThree text-darkTwo dark:text-lightTwo">
-        <section className="max-w-7xl mx-auto">
-          <h1 className="text-2xl md:text-4xl font-bold text-center text-gray-800 dark:text-gray-200">
-            Recovered Items
-          </h1>
 
-          {items?.length === 0 ? (
-            <div className="max-w-lg mx-auto mt-14 rounded-full overflow-hidden">
-              <DataNotFound />
+      <Container>
+        <h1 className="text-2xl md:text-4xl font-bold text-center text-gray-800 dark:text-gray-200">
+          Recovered Items
+        </h1>
+
+        {items?.length === 0 ? (
+          <div className="max-w-lg mx-auto mt-14 rounded-full overflow-hidden">
+            <DataNotFound />
+          </div>
+        ) : (
+          <div className="mt-8">
+            <div className="flex items-center justify-end gap-x-3">
+              <button
+                onClick={() => setIsGrid(!isGrid)}
+                className={`btn btn-ghost border border-gray-300 dark:border-gray-600 ${
+                  isGrid && "bg-gray-200 dark:bg-gray-600"
+                }`}
+              >
+                <BsFillGrid3X3GapFill />
+              </button>
+              <button
+                onClick={() => setIsGrid(!isGrid)}
+                className={`btn btn-ghost border border-gray-300 dark:border-gray-600 ${
+                  !isGrid && "bg-gray-200 dark:bg-gray-600"
+                }`}
+              >
+                <FaBars />
+              </button>
             </div>
-          ) : (
-            <div className="mt-8">
-              <div className="flex items-center justify-end gap-x-3">
-                <button
-                  onClick={() => setIsGrid(!isGrid)}
-                  className={`btn btn-ghost border border-gray-300 dark:border-gray-600 ${
-                    isGrid && "bg-gray-200 dark:bg-gray-600"
-                  }`}
-                >
-                  <BsFillGrid3X3GapFill />
-                </button>
-                <button
-                  onClick={() => setIsGrid(!isGrid)}
-                  className={`btn btn-ghost border border-gray-300 dark:border-gray-600 ${
-                    !isGrid && "bg-gray-200 dark:bg-gray-600"
-                  }`}
-                >
-                  <FaBars />
-                </button>
+            {isGrid ? (
+              <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {items.map((item, i) => (
+                  <RecoveredCard key={i} item={item} />
+                ))}
               </div>
-              {isGrid ? (
-                <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-4 gap-4">
-                  {items.map((item, i) => (
-                    <RecoveredCard key={i} item={item} />
-                  ))}
-                </div>
-              ) : (
-                <div className="overflow-x-auto mt-5">
-                  <table className="table table-zebra">
-                    {/* head */}
-                    <thead className="bg-base-300 text-darkTwo dark:text-lightTwo text-sm">
-                      <tr>
-                        <th>#</th>
-                        <th>Recovered Person</th>
-                        <th>Items Details</th>
-                        <th>Recovered Date & Location</th>
-                        <th>Type & Status</th>
-                        <th className="flex justify-center">Action</th>
-                      </tr>
-                    </thead>
+            ) : (
+              <div className="overflow-x-auto mt-5">
+                <table className="table table-zebra">
+                  {/* head */}
+                  <thead className="bg-base-300 text-darkTwo dark:text-lightTwo text-sm">
+                    <tr>
+                      <th>#</th>
+                      <th>Recovered Person</th>
+                      <th>Items Details</th>
+                      <th>Recovered Date & Location</th>
+                      <th>Type & Status</th>
+                      <th className="flex justify-center">Action</th>
+                    </tr>
+                  </thead>
 
-                    {/* table body */}
-                    <tbody>
-                      {items.map((item, i) => (
-                        <RecoveredItemTableRow key={i} item={item} i={i} />
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              )}
-            </div>
-          )}
-        </section>
-      </section>
-    </section></>
+                  {/* table body */}
+                  <tbody>
+                    {items.map((item, i) => (
+                      <RecoveredItemTableRow key={i} item={item} i={i} />
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            )}
+          </div>
+        )}
+      </Container>
+    </>
   );
 };
 
